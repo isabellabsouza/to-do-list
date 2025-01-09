ce63c39fd18ec2e99510fbffa67492c79040ff5d
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2025-01-09 20:40:33 -0300

    sequelize database connection

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<!-- <script>
+import { sequelize } from './server/api/model/db.js';
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+</script> -->
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+
+    </div>
 </template>
