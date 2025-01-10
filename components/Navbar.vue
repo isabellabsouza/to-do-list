b5e9ec7558ee4351a3a9ab9f9883c56d64c61840
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2025-01-10 20:52:24 -0300

    navbar links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
     <nav class="navbar sticky-top bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
+            <NuxtLink class="navbar-brand" to="/">
                 <img src="../public/logo.png" alt="Logo" width="25" class="d-inline-block align-text-center"/>
                 SUPER TASK
-            </a>
+            </NuxtLink>
 
-            <a class="nav-link" href="#">Login</a>
+            <NuxtLink class="nav-link" to="/auth/login">Login</NuxtLink>
         </div>
     </nav>
 </template>
